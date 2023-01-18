5eb8e27243fed784dc2deed5834c7761a2aa2003
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2023-01-18 12:05:52 +0700

    Add absolute path

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import {BrowserRouter , Routes , Route} from 'react-router-dom';
-import { FrontPage } from './pages/FrontPage';
-import { RegistrationPage } from './pages/RegistrationPage';
+import { FrontPage } from '@/pages/FrontPage';
+import { RegistrationPage } from '@/pages/RegistrationPage';
 function App() {
 
   return (
