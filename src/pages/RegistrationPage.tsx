693ac9553f6a801e537a824d38f72f2d3a071063
Faan <rudfanno1@gmail.com>
2023-01-22 19:58:08 +0700

    fea)

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,3 +1,4 @@
+import FileUploaderComponent from '@/components/FileUploaderComponent';
 import { Link } from 'react-router-dom';
 export const RegistrationPage = () => {
     return(
@@ -8,6 +9,16 @@ export const RegistrationPage = () => {
                     <Link to="/" className="bg-blue-500 rounded-full px-3 py-2 text-white">
                         <button>Home</button>
                     </Link>
+                    <FileUploaderComponent 
+                        name="hello"
+                        label="สำเนาบัตรประจำตัวประชาชน หรือ สำเนาบัตรนักเรียน"
+                        fileType="pdf"
+                    />
+                    <FileUploaderComponent 
+                        name="hello"
+                        label="ภาพถ่ายอิสระของผู้สมัครที่เห็นใบหน้าชัดเจน และมีขนาดไม่ต่ำกว่า 4 x 6 นิ้ว "
+                        fileType="image"
+                    />
                 </div>
             </div>
         </>
