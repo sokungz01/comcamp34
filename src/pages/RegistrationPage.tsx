f89c30db756d908846b11713bc40bb812028da31
Faan <rudfanno1@gmail.com>
2023-01-22 20:39:30 +0700

    feat(image-file-input-box)

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -9,16 +9,6 @@ export const RegistrationPage = () => {
                     <Link to="/" className="bg-blue-500 rounded-full px-3 py-2 text-white">
                         <button>Home</button>
                     </Link>
-                    <FileUploaderComponent 
-                        name="hello"
-                        label="สำเนาบัตรประจำตัวประชาชน หรือ สำเนาบัตรนักเรียน"
-                        fileType="pdf"
-                    />
-                    <FileUploaderComponent 
-                        name="hello"
-                        label="ภาพถ่ายอิสระของผู้สมัครที่เห็นใบหน้าชัดเจน และมีขนาดไม่ต่ำกว่า 4 x 6 นิ้ว "
-                        fileType="image"
-                    />
                 </div>
             </div>
         </>
