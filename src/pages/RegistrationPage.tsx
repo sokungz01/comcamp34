103fb348ddb073e7409280046e526b50a53bd872
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2023-01-31 21:54:40 +0700

    build(test-deploy)

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,14 +1,10 @@
 import { Link } from "react-router-dom";
+import { PersonalInfoForm } from "@/components/registrationForm/PersonalInfoForm";
 export const RegistrationPage = () => {
    return (
       <>
-         <div className='flex'>
-            <div className='m-auto'>
-               <p className='mb-3 text-xl'>Welcome to Registration Page</p>
-               <Link to='/' className='bg-blue-500 rounded-full px-3 py-2 text-white'>
-                  <button>Home</button>
-               </Link>
-            </div>
+         <div className="w-full flex justify-center">
+            <PersonalInfoForm/>
          </div>
       </>
    );
