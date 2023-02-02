0698ea827e82d08d31664b28d52a2e286108117a
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2023-02-02 21:54:30 +0700

    build(aws-test-build-2)

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-export const FrontPage = () => {
+export const RegistrationPage = () => {
     return(
         <>
            <div className="flex h-screen">
