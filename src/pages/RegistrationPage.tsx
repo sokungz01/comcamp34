2b08afda2f7d73699bc269c72211fbd83d856d41
Panurut Sirinapaisan <9.panuruj13@gmail.com>
2023-02-02 21:52:11 +0700

    build(aws-test-build)

--- src/pages/RegistrationPage.tsx
+++ src/pages/RegistrationPage.tsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom";
-import { PersonalInfoForm } from "@/components/registrationForm/PersonalInfoForm";
-export const RegistrationPage = () => {
-   return (
-      <>
-         <div className="w-full flex justify-center">
-            <PersonalInfoForm/>
-         </div>
-      </>
-   );
-};
+import { Link } from 'react-router-dom';
+export const FrontPage = () => {
+    return(
+        <>
+           <div className="flex h-screen">
+                <div className="m-auto text-center">
+                    <p className="mb-3 text-xl">Welcome to Landing Page</p>
+                    <Link to="/" className="bg-blue-500 rounded-full px-3 py-2 text-white">
+                        <button>Home</button>
+                    </Link>
+                </div>
+            </div>
+        </>
+    );
+}
