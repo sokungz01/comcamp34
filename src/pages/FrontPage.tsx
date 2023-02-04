9df74ee11306760e4f77c464c9e8bd24c3642d19
wit03 <witjarukit@gmail.com>
2023-02-04 12:55:04 +0700

    align landingPage and added static landingsection

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -3,19 +3,17 @@ import { Link } from 'react-router-dom';
 import 'aos/dist/aos.css';
 import AOS from 'aos';
 import React,{useEffect} from "react";
+import Navbar from '@/components/registrationForm/Navbar';
+import LandingSection from '@/components/frontPage/LandingSection';
 export const FrontPage = () => {
     useEffect(() => {
         AOS.init();
       }, [])
     return(
         <>
+        <Navbar/>
            <div className="flex h-screen flex-col">
-                <div className="m-auto text-center">
-                    <p className="mb-3 text-xl">Welcome to Landing Page</p>
-                    <Link to="/registration" className="bg-blue-500 rounded-full px-3 py-2 text-white">
-                        <button>Registration</button>
-                    </Link>
-                </div>
+                <LandingSection/>
                 <ConditionsSection/> 
             </div>
         </>
