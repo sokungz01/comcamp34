9ebfbcaed06efc67e8f6f92e67c7e78c3823ca9a
BoMoMoBo <boom.kanteng@hotmail.com>
2023-02-04 21:23:26 +0700

    Co-authored-by: Encall <Encall@users.noreply.github.com> Co-authored-by: Panurut Sirinapaisan <9.panuruj13@gmail.com> Co-authored-by: Chaiyapat Oam <ChaiyapatOam@users.noreply.github.com>

--- src/components/registrationForm/ParentDataForm.tsx
+++ src/components/registrationForm/ParentDataForm.tsx
@@ -17,26 +17,25 @@ export const ParentDataForm = () => {
 
    const [emergency_fname, setemergency_fname] = useState<string>("");
    const [emergency_pname, setemergency_pname] = useState();
-   const [emergency_mname, setemergency_mname] =useState<string>("");
-   const [emergency_sname, setemergency_sname] =useState<string>("");
-   const [emergency_relation, setemergency_relation] =useState<string>("");
-   const [emergency_mobile, setemergency_mobile] =useState<number>();
-   const [emergency_email, setemergency_email] =useState();
+   const [emergency_mname, setemergency_mname] = useState<string>("");
+   const [emergency_sname, setemergency_sname] = useState<string>("");
+   const [emergency_relation, setemergency_relation] = useState<string>("");
+   const [emergency_mobile, setemergency_mobile] = useState<number>();
+   const [emergency_email, setemergency_email] = useState();
 
-    useEffect(()=>{
-        if(sameParent){
-            console.log("33333333333333333")
+   useEffect(() => {
+      if (sameParent) {
+         console.log(parent_mobile);
 
-            setemergency_fname(parent_fname)
-            setemergency_pname(parent_pname)
-            setemergency_mname(parent_mname)
-            setemergency_sname(parent_sname)
-            setemergency_relation(parent_relation)
-            setemergency_mobile(parent_mobile)
-            setemergency_email(parent_email)
-         }
-    }
-    )
+         setemergency_fname(parent_fname);
+         setemergency_pname(parent_pname);
+         setemergency_mname(parent_mname);
+         setemergency_sname(parent_sname);
+         setemergency_relation(parent_relation);
+         setemergency_mobile(parent_mobile);
+         setemergency_email(parent_email);
+      }
+   });
    return (
       <>
          <div className='bg-red1 bg-opacity-10 w-full rounded-xl flex flex-col font-bai-jamjuree px-16 py-4 mx-8'>
@@ -53,19 +52,39 @@ export const ParentDataForm = () => {
                      ]}
                      required
                      setObj={setparent_pname}
-                     obj={parent_pname} 
-                    
+                     obj={parent_pname}
                   />
                </div>
                <div className='flex flex-row w-5/6'>
                   <div className='mx-2 w-full'>
-                     <Inputbox label='ชื่อ' name='parent_fname' placeholder='ธนาธร' required obj={parent_fname} setObj={setparent_fname} />
+                     <Inputbox
+                        label='ชื่อ'
+                        name='parent_fname'
+                        placeholder='ธนาธร'
+                        required
+                        obj={parent_fname}
+                        setObj={setparent_fname}
+                     />
                   </div>
                   <div className='mx-2 w-full'>
-                     <Inputbox label='ชื่อกลาง (ถ้ามี)' name='parent_mname' placeholder='' required obj={parent_mname} setObj={setparent_mname} />
+                     <Inputbox
+                        label='ชื่อกลาง (ถ้ามี)'
+                        name='parent_mname'
+                        placeholder=''
+                        required
+                        obj={parent_mname}
+                        setObj={setparent_mname}
+                     />
                   </div>
                   <div className='mx-2 w-full'>
-                     <Inputbox label='นามสกุล' name='parent_sname' placeholder='' required obj={parent_sname} setObj={setparent_sname}/>
+                     <Inputbox
+                        label='นามสกุล'
+                        name='parent_sname'
+                        placeholder=''
+                        required
+                        obj={parent_sname}
+                        setObj={setparent_sname}
+                     />
                   </div>
                </div>
             </div>
@@ -78,25 +97,46 @@ export const ParentDataForm = () => {
                         placeholder=''
                         required
                         obj={parent_relation}
-                        setObj={setparent_relation}   
-                            />
+                        setObj={setparent_relation}
+                     />
                   </div>
                   <div className='w-full ml-2'>
-                     <TelInput label='เบอร์โทรศัพท์' name='parent_mobile' placeholder='' required obj={parent_mobile} setobj={setparent_mobile}/>
+                     <TelInput
+                        label='เบอร์โทรศัพท์'
+                        name='parent_mobile'
+                        placeholder=''
+                        required
+                        obj={parent_mobile}
+                        setobj={setparent_mobile}
+                     />
                   </div>
                   <div className='w-full ml-2'>
-                     <EmailInput label='Email' name='parent_email' placeholder='' obj={parent_email} setObj={setparent_email}/>
+                     <EmailInput
+                        label='Email'
+                        name='parent_email'
+                        placeholder=''
+                        obj={parent_email}
+                        setObj={setparent_email}
+                     />
                   </div>
                </div>
             </div>
 
+            <br></br>
 
-                     <br></br>
-
-            <h4 className='text-4xl text-red2 font-semibold mb-2'> ผู้ปกครองที่ติดต่อได้ในกรณีฉุกเฉิน </h4>
+            <h4 className='text-4xl text-red2 font-semibold mb-2'>
+               {" "}
+               ผู้ปกครองที่ติดต่อได้ในกรณีฉุกเฉิน{" "}
+            </h4>
             <div className='flex mt-4'>
                <p className='mr-2'> เหมือนกับผู้ปกครอง </p>
-               <CheckboxInput onclick={()=>(sameParent ? setsameParent(false) : setsameParent(true))} setobj={setsameParent} obj={sameParent} name='same_parent' label=' ' />
+               <CheckboxInput
+                  onclick={() => (sameParent ? setsameParent(false) : setsameParent(true))}
+                  setobj={setsameParent}
+                  obj={sameParent}
+                  name='same_parent'
+                  label=' '
+               />
                <p className='ml-2'> </p>
             </div>
             <div className='flex mt-4'>
@@ -112,44 +152,74 @@ export const ParentDataForm = () => {
                      required
                      setObj={setemergency_pname}
                      obj={emergency_pname}
-                   
-
                   />
                </div>
                <div className='flex flex-row w-5/6'>
                   <div className='mx-2 w-full'>
-                     <Inputbox label='ชื่อ' name='emergency_fname' placeholder='ธนาธร' required  setObj={setemergency_fname} obj={emergency_fname} />
+                     <Inputbox
+                        label='ชื่อ'
+                        name='emergency_fname'
+                        placeholder='ธนาธร'
+                        required
+                        setObj={setemergency_fname}
+                        obj={emergency_fname}
+                     />
                   </div>
                   <div className='mx-2 w-full'>
-                     <Inputbox label='ชื่อกลาง (ถ้ามี)' name='emergency_mname' placeholder='' required setObj={setemergency_mname} obj={emergency_mname}/>
+                     <Inputbox
+                        label='ชื่อกลาง (ถ้ามี)'
+                        name='emergency_mname'
+                        placeholder=''
+                        required
+                        setObj={setemergency_mname}
+                        obj={emergency_mname}
+                     />
                   </div>
                   <div className='mx-2 w-full'>
-                     <Inputbox label='นามสกุล' name='emergency_sname' placeholder='' required setObj={setemergency_sname} obj={emergency_sname} />
+                     <Inputbox
+                        label='นามสกุล'
+                        name='emergency_sname'
+                        placeholder=''
+                        required
+                        setObj={setemergency_sname}
+                        obj={emergency_sname}
+                     />
                   </div>
                </div>
             </div>
             <div className='flex  mt-2'>
-               
-                  <div className='w-full mr-2'>
-                     <Inputbox
-                        label='ความสัมพันธ์'
-                        name='emergency_relation'
+               <div className='w-full mr-2'>
+                  <Inputbox
+                     label='ความสัมพันธ์'
+                     name='emergency_relation'
+                     placeholder=''
+                     required
+                     setObj={setemergency_relation}
+                     obj={emergency_relation}
+                  />
+               </div>
+               <div className='flex justify-center'>
+                  <div className='w-full ml-2'>
+                     <TelInput
+                        label='เบอร์โทรศัพท์'
+                        name='emergency_mobile'
                         placeholder=''
                         required
-                        setObj={setemergency_relation}
-                        obj={emergency_relation}
+                        setobj={setemergency_mobile}
+                        obj={emergency_mobile}
                      />
                   </div>
-                  <div className='flex justify-center'>
                   <div className='w-full ml-2'>
-                     <TelInput label='เบอร์โทรศัพท์' name='emergency_mobile' placeholder='' required setobj={setemergency_mobile} obj={emergency_mobile} />
-                  </div>   
-                  <div className='w-full ml-2'>
-                     <EmailInput label='Email' name='emergency_email' placeholder='' setObj={setemergency_email} obj={emergency_email}/>
-                  </div>   
+                     <EmailInput
+                        label='Email'
+                        name='emergency_email'
+                        placeholder=''
+                        setObj={setemergency_email}
+                        obj={emergency_email}
+                     />
+                  </div>
                </div>
             </div>
-
          </div>
       </>
    );
