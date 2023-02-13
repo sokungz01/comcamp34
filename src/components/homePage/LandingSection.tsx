52dc1021969c8d6202273ca7757a21da5827b512
SoKuNGz01 <9.panuruj13@gmail.com>
2023-02-13 23:17:16 +0700

    :ambulance:HOTFIX : Registration button z-index on mobile

--- src/components/homePage/LandingSection.tsx
+++ src/components/homePage/LandingSection.tsx
@@ -21,7 +21,7 @@ const LandingSection: React.FC = () => {
                > */}
                <button
                   onClick={ComingSoonSwal}
-                  className='z-[9999] inline-block mx-auto lg:text-3xl lg:font-normal px-8 py-2 leading-none text-base-white bg-red2 lg:border-2 lg:border-red2 my-4 lg:mt-8 rounded-xl'
+                  className='z-[998] inline-block mx-auto lg:text-3xl lg:font-normal px-8 py-2 leading-none text-base-white bg-red2 lg:border-2 lg:border-red2 my-4 lg:mt-8 rounded-xl'
                >
                   Register
                </button>
