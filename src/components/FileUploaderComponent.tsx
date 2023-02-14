e93e6fe909a4d508ee8505bc365566fe7ecfee66
SoKuNGz01 <9.panuruj13@gmail.com>
2023-02-14 21:24:08 +0700

    :adhesive_bandage: FIX : Download button size

--- src/components/FileUploaderComponent.tsx
+++ src/components/FileUploaderComponent.tsx
@@ -112,7 +112,7 @@ const FileUploaderComponent = ({
                   <a
                      href={downloadURL}
                      target='_blank'
-                     className='w-full sm:w-[26%] lg:w-[70%] xl:w-[50%] mb-4 lg:mb-0 hover:bg-blue2 bg-blue1 rounded-lg text-white font-teko tracking-wider py-0.5 lg:mr-0'
+                     className='w-full sm:w-[26%] lg:w-[50%] xl:w-[40%] xl:w-[40%] mb-4 lg:mb-0 hover:bg-blue2 bg-blue1 rounded-lg text-white font-teko tracking-wider py-0.5 lg:mr-0'
                   >
                      <div className='flex flex-row justify-center items-center'>
                         <svg
@@ -127,7 +127,7 @@ const FileUploaderComponent = ({
                               clipRule='evenodd'
                            />
                         </svg>
-                        <p>Download</p>
+                        <p className="mt-1">Download</p>
                      </div>
                   </a>
                ) : null}
@@ -172,7 +172,7 @@ const FileUploaderComponent = ({
                                  d='M12 19.5v-15m0 0l-6.75 6.75M12 4.5l6.75 6.75'
                               />
                            </svg>
-                           <p>Upload File</p>
+                           <p  className="mt-1">Upload File</p>
                         </div>
                      </button>
                   </>
@@ -208,7 +208,7 @@ const FileUploaderComponent = ({
                                        d='M12 21a9.004 9.004 0 008.716-6.747M12 21a9.004 9.004 0 01-8.716-6.747M12 21c2.485 0 4.5-4.03 4.5-9S14.485 3 12 3m0 18c-2.485 0-4.5-4.03-4.5-9S9.515 3 12 3m0 0a8.997 8.997 0 017.843 4.582M12 3a8.997 8.997 0 00-7.843 4.582m15.686 0A11.953 11.953 0 0112 10.5c-2.998 0-5.74-1.1-7.843-2.918m15.686 0A8.959 8.959 0 0121 12c0 .778-.099 1.533-.284 2.253m0 0A17.919 17.919 0 0112 16.5c-3.162 0-6.133-.815-8.716-2.247m0 0A9.015 9.015 0 013 12c0-1.605.42-3.113 1.157-4.418'
                                     />
                                  </svg>
-                                 <p>Preview File</p>
+                                 <p className="mt-1">Preview File</p>
                               </div>
                            </button>
                         </a>
